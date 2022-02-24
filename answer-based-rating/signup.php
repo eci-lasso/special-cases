ad74457391c013265fa6a83fe97055c2e853573e
eci-lasso <66796419+eci-lasso@users.noreply.github.com>
2022-02-24 16:41:49 -0500

    Update signup.php

--- answer-based-rating/signup.php
+++ answer-based-rating/signup.php
@@ -3,23 +3,16 @@
 require('src/LassoLead.php');
 require('src/RegistrantSubmitter.php');
 
-/* These variables should only be attached on the server side
- * and should not be hidden fields on the registration form
- * Note that $apiKey is where the Lasso UID is placed.
- */
 $clientId  = '1111';
 $projectId = '1001';
 $apiKey = '1x1x1';
 
 if (empty($clientId) || empty($projectId) || empty($apiKey)){
-	throw new Exception('Required parameters are not set, please
-				check that your $clientId, $projectId and $apiKey are
-				configured correctly');
+	throw new Exception('Required parameters are not set, please check that
+				your $clientId, $projectId and $apiKey
+				are configured correctly');
 }
 
-/* Constructing and submitting a lead:
- * Map form fields to the lead object and submit
- */
 $lead = new LassoLead(
 	$_REQUEST['FirstName'],
 	$_REQUEST['LastName'],
