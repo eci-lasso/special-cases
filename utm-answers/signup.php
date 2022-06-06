08adc6b39ad055293e034c2ac4a52e5280966bad
ECI | Lasso <66796419+eci-lasso@users.noreply.github.com>
2022-06-06 11:58:23 -0400

    Update signup.php

--- utm-answers/signup.php
+++ utm-answers/signup.php
@@ -9,30 +9,32 @@ $apiKey = '1x1x1';
 
 if (empty($clientId) || empty($projectId) || empty($apiKey)){
 	throw new Exception('Required parameters are not set, please check that
-				your $clientId, $projectId and $apiKey
-				are configured correctly');
+							your $clientId, $projectId and $apiKey
+							are configured correctly');
 }
 
 $lead = new LassoLead(
 	$_REQUEST['FirstName'],
-	$_REQUEST['LastName'],
-	$projectId,
-	$clientId
+    $_REQUEST['LastName'],
+    $projectId,
+    $clientId
 );
 
 $lead->addPhone($_REQUEST['Phone']);
 
 $lead->addEmail($_REQUEST['Email']);
 
-/* UTM Information
+/* Questions (text answer)
  *
- * For capturing UTM information and passing to Lasso as answers to manual input questions
+ * Only for questions that require text input answers
  * $lead->answerQuestionByIdForText($questionId,$_REQUEST['Questions'][$questionId]);
  */
-$lead->answerQuestionByIdForText(1111, $_REQUEST['UTMCampaign']);
-$lead->answerQuestionByIdForText(2222, $_REQUEST['UTMContent']);
-$lead->answerQuestionByIdForText(3333, $_REQUEST['UTMSource']);
-$lead->answerQuestionByIdForText(4444, $_REQUEST['UTMTerm']);
+$lead->answerQuestionByIdForText(107615, $_REQUEST['utm_campaign']);
+$lead->answerQuestionByIdForText(107616, $_REQUEST['utm_content']);
+$lead->answerQuestionByIdForText(132009, $_REQUEST['utm_id']);
+$lead->answerQuestionByIdForText(132008, $_REQUEST['utm_medium']);
+$lead->answerQuestionByIdForText(107617, $_REQUEST['utm_source']);
+$lead->answerQuestionByIdForText(107618, $_REQUEST['utm_term']);
 
 $lead->sendAssignmentNotification();
 
