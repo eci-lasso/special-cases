c5c695fcd3098cbcaa4d8d830e0021effd3b58ff
Sapna <54601914+eci-sapna@users.noreply.github.com>
2022-12-22 10:42:08 -0500

    Update signup.php

--- answer-based-projects/signup.php
+++ answer-based-projects/signup.php
@@ -7,9 +7,9 @@ $clientId  = '1111';
 $apiKey = '1x1x1';
 
 if (empty($clientId) || empty($apiKey)){
-	throw new Exception('Required parameters are not set, please check that
-				your $clientId and $apiKey
-				are configured correctly');
+  throw new Exception('Required parameters are not set, please check that
+                        your $clientId and $apiKey
+                        are configured correctly');
 }
 
 $projects = [];
@@ -17,42 +17,42 @@ $projects = [];
 /* Associating projects to answers
  * 
  * foreach($_REQUEST['Questions'][$questionId] as $index => $answer){
- * 	switch($answer) {
- * 		case $answerId:
- * 			$projects[] = $projectId;
- * 			break;
- * 		case $answerId:
- * 			$projects[] = $projectId;
- * 			break;
- * 		case $answerId:
- * 			$projects[] = $projectId;
- * 			break;
- * 		default:
- * 			break;
- * 	}
+ *   switch($answer) {
+ *     case $answerId:
+ *       $projects[] = $projectId;
+ *       break;
+ *     case $answerId:
+ *       $projects[] = $projectId;
+ *       break;
+ *     case $answerId:
+ *       $projects[] = $projectId;
+ *       break;
+ *     default:
+ *       break;
+ *   }
  * }
  */
 foreach($_REQUEST['Questions'][1111] as $index => $answer){
-	switch($answer) {
-		case 101:
-			$projects[] = 1111;
-			break;
-		case 102:
-			$projects[] = 2222;
-			break;
-		case 103:
-			$projects[] = 3333;
-			break;
-		default:
-			break;
-	}
+  switch($answer) {
+    case 101:
+      $projects[] = 1111;
+      break;
+    case 102:
+      $projects[] = 2222;
+      break;
+    case 103:
+      $projects[] = 3333;
+      break;
+    default:
+      break;
+  }
 }
 
 $lead = new LassoLead(
-	$_REQUEST['FirstName'],
-	$_REQUEST['LastName'],
-	$projects[0],
-	$clientId
+  $_REQUEST['FirstName'],
+  $_REQUEST['LastName'],
+  $projects[0],
+  $clientId
 );
 
 /* Projects
@@ -60,10 +60,10 @@ $lead = new LassoLead(
  * For submitting to selected projects
  */
 foreach($projects as $index => $projectId){
-	if ($index == 0){
-		continue;
-	}
-	$lead->addProject($projectId);
+  if ($index == 0){
+    continue;
+  }
+  $lead->addProject($projectId);
 }
 
 /* Questions (select answer)
@@ -71,7 +71,7 @@ foreach($projects as $index => $projectId){
  * For any number of questions where answers are selected
  */
 foreach($_REQUEST['Questions'] as $questionId => $value){
-	 $lead->answerQuestionById($questionId, $value);
+  $lead->answerQuestionById($questionId, $value);
 }
 
 $lead->addPhone($_REQUEST['Phone']);
